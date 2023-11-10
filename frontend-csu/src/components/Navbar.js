280df23b5d270c4ea7c9fb6da3b91e15209f64f2
Matei <mateimurarasu@yahoo.com>
2023-11-10 20:52:02 +0200

    Add base react components including finished Navbar Component

---
+++ frontend-csu/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "./componentsUI/Shared.css";
+import "./componentsUI/Navbar.css";
+
+export default function Navbar() {
+   return (
+      <nav className="navbar">
+         <div className="navbar-element">
+            <div className="navbar-element__image"></div>
+            <div className="navbar-element__title"></div>
+         </div>
+         <div className="navbar-element">
+            <div className="navbar-element__link"></div>
+            <div className="navbar-element__link"></div>
+            <div className="navbar-element__link"></div>
+         </div>
+      </nav>
+   );
+}
