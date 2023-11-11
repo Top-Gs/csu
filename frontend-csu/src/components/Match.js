bf708d3dd8b8869f6b7a7714d575e99bf6d932f9
Matei <mateimurarasu@yahoo.com>
2023-11-11 12:17:48 +0200

    Create Match component in React.

---
+++ frontend-csu/src/components/Match.js
@@ -0,0 +1,14 @@
+import "./componentsUI/Match.css";
+
+export default function Match() {
+   return (
+      <div className="match-wrapper">
+         <p className="match-type"></p>
+         <div className="match-summary"></div>
+         <div className="match-teams">
+            <div className="match-teams__team"></div>
+            <div className="match-teams__team"></div>
+         </div>
+      </div>
+   );
+}
