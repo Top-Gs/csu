43f03c9de7b5e355f9a26a4fa1591eb339032a2b
Matei <mateimurarasu@yahoo.com>
2023-11-11 11:04:22 +0200

    Add PageTitle react component

---
+++ frontend-csu/src/components/PageTitle.js
@@ -0,0 +1,11 @@
+// TODO -> Complete this and CSS
+import "./componentsUI/PageTitle.css";
+
+export default function PageTitle() {
+   return (
+      <div className="page-wrapper">
+         <div className="title-logo"></div>
+         <div className="title-text"></div>
+      </div>
+   );
+}
