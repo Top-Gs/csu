079d81e06ab0d0716119f299144895379befe006
Matei <mateimurarasu@yahoo.com>
2023-11-11 11:04:58 +0200

    Implement PageTitle component into base react app.

--- frontend-csu/src/App.js
+++ frontend-csu/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import PageTitle from "./components/PageTitle";
 function App() {
    return (
       <div className="App">
          <Navbar />;
+         <PageTitle />;
          <Footer />;
       </div>
    );
