46750aca42315fa8ab5fa61e596cb93910da3ff2
Matei <mateimurarasu@yahoo.com>
2023-11-13 13:17:11 +0200

    Add NewsCard react component.

--- frontend-csu/src/components/NewsCard.js
+++ frontend-csu/src/components/NewsCard.js
@@ -1,1 +1,17 @@
-// TODO -> News card component. Create this and implement CSS accordingly to the wireframe design.
+import "./componentsUI/NewsCard.css";
+
+export default function NewsCard() {
+   return (
+      <div className="news-card-container">
+         <div src="" className="news-card__image"></div>
+         <div className="news-card-summary-items">
+            <div className="news-card-summary-items__item"></div>
+            <div className="news-card-summary-items__item"></div>
+         </div>
+         <div className="news-card-text">
+            <p className="news-card-text__title"></p>
+            <p className="news-card-text__headline"></p>
+         </div>
+      </div>
+   );
+}
