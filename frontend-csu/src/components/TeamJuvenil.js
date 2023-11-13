7ecd6b6713c03b400059ed5b41e1a1a22fe1dca7
Matei <mateimurarasu@yahoo.com>
2023-11-13 13:28:50 +0200

    Create TeamJuvenil and TeamSeniori react components.

---
+++ frontend-csu/src/components/TeamJuvenil.js
@@ -0,0 +1,10 @@
+import "./componentsUI/TeamComponent.css";
+
+export default function TeamJuvenil() {
+   return (
+      <div className="team-juvenil-container">
+         <div className="team-juvenil__image"></div>
+         <p className="team-juvenil__summary"></p>
+      </div>
+   );
+}
---
+++ frontend-csu/src/components/TeamSeniori.js
@@ -0,0 +1,10 @@
+import "./componentsUI/TeamComponent.css";
+
+export default function TeamSeniori() {
+   return (
+      <div className="team-seniori-container">
+         <div className="team-seniori__image"></div>
+         <p className="team-seniori__summary"></p>
+      </div>
+   );
+}
