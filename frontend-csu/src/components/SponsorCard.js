e493aa10233caaa6886de649f379f099319a7de8
Matei <mateimurarasu@yahoo.com>
2023-11-13 14:12:33 +0200

    Create SponsorCard react component.

--- frontend-csu/src/components/SponsorCard.js
+++ frontend-csu/src/components/SponsorCard.js
@@ -1,1 +1,10 @@
-// TODO -> Sponsors component. Create this and implement CSS accordingly to the wireframe design.
+import "./componentsUI/SponsorCard.css";
+/*
+NOTE :  Trebuie sa fie create pentru doua tipuri de logo, pentru ca nu toate firmele au logo in aceleasi dimensiuni. In momentul de fata exista 7 firme de sponsori care au nevoie de card, din care doar 2 au nevoie de dimensiuni sub forma de patrat, restul find rectangulare. 
+NOTE : Posibil sa fie schimbate pe viitor atunci cand vom avea imaginile in format mai calitativ sau vom gasi o alternativa mai buna, insa pana atunci avem nevoie de 2 tipuri de carduri.
+Am nevoie de consultatii de la Beni sau Danicu sau Tudor 
+*/
+
+export default function SponsorCard() {
+   return <div className="sponsor-card"></div>;
+}
