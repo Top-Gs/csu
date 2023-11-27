afe9714588e5b03976fa123657041f70f901a7e6
Matei <mateimurarasu@yahoo.com>
2023-11-27 16:14:15 +0200

    Create news page

--- frontend-csu/src/pages/NewsPage.js
+++ frontend-csu/src/pages/NewsPage.js
@@ -8,3 +8,31 @@
 // - Cardul principal care apare primul cu informatiile zilei, in care vom avea stirea cea mai importanta.
 // - Cardurile secundare, care vor avea stirie secundare, sau "others".
 // NOTE : Amandoua tipuri de carduri trebuie onClick sa fie scalate in viewport. Background blurat? Overlay intunecat? Ask Dani.
+
+import "./pagesUI/NewsPage.css";
+import "../components/componentsUI/Shared.css";
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import MainNewsCard from "../components/MainNewsCard";
+import NewsCard from "../components/NewsCard";
+import SponsorSidebar from "../components/SponsorSidebar";
+
+export default function NewsPage() {
+   return (
+      <div className="news-container">
+         <Navbar />
+         <div className="content-container">
+            <MainNewsCard />
+            <section className="news-card-grid">
+               <NewsCard />
+               <NewsCard />
+               <NewsCard />
+               <NewsCard />
+            </section>
+         </div>
+         <SponsorSidebar />
+         <Footer />
+      </div>
+   );
+}
