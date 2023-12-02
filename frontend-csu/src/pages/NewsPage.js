a142ca8704443cad76e0488c89fe2432e8bcd43a
Matei <mateimurarasu@yahoo.com>
2023-12-02 12:15:10 +0200

    Update NewsPage logic.

--- frontend-csu/src/pages/NewsPage.js
+++ frontend-csu/src/pages/NewsPage.js
@@ -18,7 +18,32 @@ import MainNewsCard from "../components/MainNewsCard";
 import NewsCard from "../components/NewsCard";
 import SponsorSidebar from "../components/SponsorSidebar";
 
+// function fetch() {
+//    let options = {
+//       method: "GET",
+//       headers: {
+//          "Content-Type": "application/json",
+//       },
+//    };
+
+//    fetch("https://localhost:7280/api/v1/news", options)
+//       .then((response) => response.json())
+//       .then((data) => {
+//          let images = data.images;
+//          let container = document.getElementById("imageContainer");
+//          images.forEach((image) => {
+//             let img = document.createElement("img");
+//             img.src = "data:image/jpeg;base64," + image;
+//             container.appendChild(img);
+//          });
+
+//          console.log(data);
+//       })
+//       .catch((error) => console.error("Error:", error));
+// }
+
 export default function NewsPage() {
+   // fetch();
    return (
       <div className="news-container">
          <Navbar />
