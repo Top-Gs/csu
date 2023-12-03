ac2d1a5a186fbbb3975f2a1df10c18b6cb861539
Matei <mateimurarasu@yahoo.com>
2023-12-03 12:03:10 +0200

    Add styling for SponsorSidebar

--- frontend-csu/src/components/SponsorSidebar.js
+++ frontend-csu/src/components/SponsorSidebar.js
@@ -3,6 +3,7 @@ import React from "react";
 
 // Style Imports
 import "./componentsUI/SponsorSidebar.css";
+import "./componentsUI/SponsorGrid.css";
 
 export default function SponsorSidebar() {
    return (
