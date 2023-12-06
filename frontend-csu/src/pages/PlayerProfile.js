e9fffed13dd1cb27686c72fe7289032491b6d336
Matei <mateimurarasu@yahoo.com>
2023-12-06 11:58:13 +0200

    Update class naming and spacing in PlayerProfile page.

--- frontend-csu/src/pages/PlayerProfile.js
+++ frontend-csu/src/pages/PlayerProfile.js
@@ -3,7 +3,6 @@ import React from "react";
 
 // Styling Imports
 import "../components/componentsUI/Shared.css";
-import "./pagesUI/AboutUs.css";
 import "./pagesUI/PlayerProfile.css";
 
 // Component Imports
@@ -14,8 +13,8 @@ import Footer from "../components/Footer";
 
 export default function PlayerProfile() {
    return (
-      <div className="player-profile-main-container">
-         <div className="csu-team-photo" src="../../res/images/csu-team.png" alt="CSU team photo." />
+      <div className="player-profile-container">
+         <div className="player-profile__team-photo" alt="CSU team photo." />
          <PlayerSummary />
          <PlayerInfoCard />
          <SponsorSidebar />
