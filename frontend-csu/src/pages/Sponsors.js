a5b15fe866fc333dfd10d34efba06e29a74aade6
Matei <mateimurarasu@yahoo.com>
2023-12-06 11:44:41 +0200

    Update classnames and styling for Sponsors page.

--- frontend-csu/src/pages/Sponsors.js
+++ frontend-csu/src/pages/Sponsors.js
@@ -2,8 +2,8 @@
 import React from "react";
 
 // Styling Imports
-import "./pagesUI/Sponsors.css";
 import "../components/componentsUI/Shared.css";
+import "./pagesUI/Sponsors.css";
 
 // Components Imports
 import Navbar from "../components/Navbar";
@@ -14,12 +14,12 @@ export default function Sponsors() {
    return (
       <div className="sponsors-container">
          <Navbar />
-         <div className="csu-team-photo" src="../../res/images/csu-team.png" alt="CSU team photo." />
-         <div className="sponsors-logo-primary-text">
-            <div className="logo" alt="logo" />
-            <h2>CLUBUL SPORTIV UNIVERSITATEA SUCEAVA</h2>
+         <div className="sponsors__team-photo" src="../../res/images/csu-team.png" alt="CSU team photo." />
+         <div className="sponsors__club-component">
+            <div className="club__logo" alt="logo" />
+            <h1 className="club__title">CLUBUL SPORTIV UNIVERSITATEA SUCEAVA</h1>
          </div>
-         <h2 className="sponsored-by">Sponsorizat de :</h2>
+         <h2 className="sponsors__sponsored-by">Sponsorizat de :</h2>
          <SponsorGrid />
          <Footer />
       </div>
