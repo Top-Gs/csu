a3f10b5e150781675c3434bed2784b1d95ce5c35
Matei <mateimurarasu@yahoo.com>
2023-12-07 09:27:21 +0200

    Update comments on NewsPage.

--- frontend-csu/src/pages/NewsPage.js
+++ frontend-csu/src/pages/NewsPage.js
@@ -8,28 +8,39 @@ import MainNewsCard from "../components/MainNewsCard";
 import NewsCard from "../components/NewsCard";
 import SponsorSidebar from "../components/SponsorSidebar";
 
-// function fetch() {
-//    let options = {
-//       method: "GET",
-//       headers: {
-//          "Content-Type": "application/json",
-//       },
-//    };
+/* TODO :
+> Cum se face Fetch in React si cum integrez?
+> Componente pentru News cu ruta catre componenta respectiva.
+> Functie de limitare a headline-urilui intr-un numar restrans de caractere
+> O cale mai bune de a integra animatii si UX?
+> Ordonarea prioritatii in functie de cea mai recenta data
+   NOTE -) Teoretic, ar trebui sa punem cea mai recenta stire dupa ce isi pierde relevanta in casuta a[0][0] din Grid.
+*/
 
-//    fetch("https://localhost:7280/api/v1/news", options)
-//       .then((response) => response.json())
-//       .then((data) => {
-//          let images = data.images;
-//          let container = document.getElementById("imageContainer");
-//          images.forEach((image) => {s
-//             img.src = "data:image/jpeg;base64," + image;
-//             container.appendChild(img);
-//          });
+// FUNCTIA DE FETCH
+/* function fetch() {
+    let options = {
+       method: "GET",
+       headers: {
+          "Content-Type": "application/json",
+       },
+    };
 
-//          console.log(data);
-//       })
-//       .catch((error) => console.error("Error:", error));
-// }
+    fetch("https://localhost:7280/api/v1/news", options)
+       .then((response) => response.json())
+       .then((data) => {
+          let images = data.images;
+          let container = document.getElementById("imageContainer");
+          images.forEach((image) => {s
+             img.src = "data:image/jpeg;base64," + image;
+             container.appendChild(img);
+          });
+
+          console.log(data);
+       })
+       .catch((error) => console.error("Error:", error));
+ }
+*/
 
 export default function NewsPage() {
    // fetch();
