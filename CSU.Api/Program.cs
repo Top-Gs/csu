dbca67d5c7cad364d65f78faffc4431aaba848ac
Albu Tudor <tudor.albu@student.usv.ro>
2023-12-23 23:56:16 +0200

    Setting up JWT support.

--- CSU.Api/Program.cs
+++ CSU.Api/Program.cs
@@ -1,3 +1,4 @@
+using CSU.Api.Installers;
 using CSU.Application;
 using CSU.Infrastructure;
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.InstallServicesInAssembly(builder.Configuration);
 
 builder.Services
     .AddApplication()
@@ -26,6 +29,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
