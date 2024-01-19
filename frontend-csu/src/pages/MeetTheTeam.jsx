403b03502fb8a7aa4ae398d7e5ad9142758f0b40
Matei <mateimurarasu@yahoo.com>
2024-01-19 11:48:13 +0200

    Implement components into MTT page.

--- frontend-csu/src/pages/MeetTheTeam.jsx
+++ frontend-csu/src/pages/MeetTheTeam.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import "../components/componentsUI/Shared.css"
+import "./pagesUI/MeetTheTeam.css"
+
+import Navbar from "../components/Navbar";
+import SponsorSidebar from "../components/SponsorSidebar";
+import Footer from "../components/Footer";
 import { TeamContainer } from '../components/TeamContainer'
+import { HeaderImage } from '../components/HeaderImage';
+
 
 export const MeetTheTeam = () => {
   return (
-    <div>
+    <div className='meet-the-team-container'>
+        <Navbar/>
+        <HeaderImage/>
         <TeamContainer/>
+        <SponsorSidebar/>
+        <Footer/>
     </div>
   )
 }
