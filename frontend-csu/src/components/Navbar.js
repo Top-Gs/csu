c6469156ec9641162d1bd5c381a9cd2f781d1994
Matei <mateimurarasu@yahoo.com>
2024-01-24 13:29:42 +0200

    Fix navbar link sizing.

--- frontend-csu/src/components/Navbar.js
+++ frontend-csu/src/components/Navbar.js
@@ -1,39 +1,30 @@
 // React Imports
-import React from "react"
+import React from "react";
 
 // Styling imports
-import "./componentsUI/Shared.css"
-import "./componentsUI/Navbar.css"
+import "./componentsUI/Shared.css";
+import "./componentsUI/Navbar.css";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar-element">
-        <div alt="logo-csu" className="navbar-element__image"></div>
-      </div>
-      <div className="navbar-element">
-        <a
-          href="http://localhost:3000/playerprofile"
-          className="navbar-element__link"
-        >
-          Acasă
-        </a>
-        <a href="http://localhost:3000/news" className="navbar-element__link">
-          Noutăți
-        </a>
-        <a
-          href="http://localhost:3000/sponsors"
-          className="navbar-element__link"
-        >
-          Sponsori
-        </a>
-        <a
-          href="http://localhost:3000/aboutus"
-          className="navbar-element__link"
-        >
-          Despre noi
-        </a>
-      </div>
-    </nav>
-  )
+   return (
+      <nav className="navbar">
+         <div className="navbar-element">
+            <div alt="logo-csu" className="navbar-element__image"></div>
+         </div>
+         <div className="navbar-element">
+            <a href="http://localhost:3000/playerprofile" className="navbar-element__link">
+               Acasă
+            </a>
+            <a href="http://localhost:3000/news" className="navbar-element__link">
+               Noutăți
+            </a>
+            <a href="http://localhost:3000/sponsors" className="navbar-element__link">
+               Sponsori
+            </a>
+            <a href="http://localhost:3000/aboutus" className="navbar-element__link">
+               Despre noi
+            </a>
+         </div>
+      </nav>
+   );
 }
