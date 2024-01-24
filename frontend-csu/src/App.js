0e913abb3e7f79c2483d84c1f89eb73348a588c6
BeniaminDumitriu <benidumitriu5@gmail.com>
2024-01-24 18:00:40 +0200

    Viata e dura

--- frontend-csu/src/App.js
+++ frontend-csu/src/App.js
@@ -23,33 +23,33 @@ import HomePage from "./pages/HomePage";
 import { MeetTheTeamJuv } from "./pages/MeetTheTeamJuv";
 
 function App() {
-   return (
-      <div className="App">
-         <Router>
-            <Routes>
-               {/* Main routes for homepage */}
-               <Route path="/" element={<NewsPage />} />
-               <Route path="/playerprofile" element={<PlayerProfile />} />
-               <Route path="/news" element={<NewsPage />} />
-               <Route path="/sponsors" element={<Sponsors />} />
-               <Route path="/aboutus" element={<AboutUs />} />
-               <Route path="/singlenews" element={<SingleNewsPage />} />
-               <Route path="/login" element={<LoginPage />} />
-               <Route path="/register" element={<RegisterPage />} />
-               <Route path="/mtt" element={<MeetTheTeam />} />
-               <Route path="/mttjuv" element={<MeetTheTeamJuv />} />
-               {/* Admin Routes */}
-               <Route path="/admin/*" element={<AdminLayout />}>
-                  <Route index element={<AdminDashboard />} />
-                  <Route path="news" element={<NewsAdm />} />
-                    <Route path="/home" element={<HomePage />} />
-                      <Route path="/matches" element={<Matches />} />
-                  {/* Add more admin routes as needed */}
-               </Route>
-            </Routes>
-         </Router>
-      </div>
-   );
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          {/* Main routes for homepage */}
+          <Route path="/" element={<NewsPage />} />
+          <Route path="/playerprofile" element={<PlayerProfile />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/singlenews" element={<SingleNewsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/mtt" element={<MeetTheTeam />} />
+          <Route path="/mttjuv" element={<MeetTheTeamJuv />} />
+          {/* Admin Routes */}
+          <Route path="/admin/*" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="news" element={<NewsAdm />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/matches" element={<Matches />} />
+            {/* Add more admin routes as needed */}
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
